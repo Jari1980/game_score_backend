d672132131e95400a7a122bdb042beddbd137301
jari.tenhunen <jari.tenhunen@outlook.com>
2026-01-10 09:39:56 +0100

    MatchRepository extending Jpa added

---
+++ src/main/java/SportScore/Sport/repository/MatchRepository.java
@@ -0,0 +1,9 @@
+package SportScore.Sport.repository;
+
+import SportScore.Sport.domain.entity.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+}
