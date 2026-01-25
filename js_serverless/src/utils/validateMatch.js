6ab1a5b7c60efdb1bd3ac1990e36a6cf2f179687
jari.tenhunen <jari.tenhunen@outlook.com>
2026-01-25 15:02:16 +0100

    Validation for postrequest and README.md updated with PostgreSql and .env info

--- js_serverless/src/utils/validateMatch.js
+++ js_serverless/src/utils/validateMatch.js
@@ -1,9 +1,27 @@
 export function validateMatch(match) {
-  if (!match.matchDate || !match.homeTeam || !match.awayTeam) {
-    throw new Error("Missing required fields");
+
+  // Check required string fields
+  if (!match.homeTeam || !match.awayTeam) {
+    throw new Error("homeTeam and awayTeam are required");
   }
+
+  // Check that string only have letters, numbers, spaces
+  const stringFields = ["homeTeam", "awayTeam"];
+  for (const field of stringFields) {
+    if (!/^[A-Za-z0-9\s]+$/.test(match[field])) {
+      throw new Error(`${field} contains invalid characters`);
+    }
+  }
+
+  // Check that matchDate exists and is a valid date
+  if (!match.matchDate || isNaN(new Date(match.matchDate).getTime())) {
+    throw new Error("matchDate is required and must be a valid date");
+  }
+
+  // Check scores
   if (typeof match.homeTeamScore !== "number" || typeof match.awayTeamScore !== "number") {
     throw new Error("Scores must be numbers");
   }
+
   return true;
 }
