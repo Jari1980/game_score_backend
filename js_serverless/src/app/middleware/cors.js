246eb550610c2254f8e9f1759476df0039b16187
Jari1980 <jari.tenhunen@outlook.com>
2026-02-28 13:23:42 +0100

    updating last pr with changes to cors for enabling PUT and DELETE request which will be used in admin pages

--- js_serverless/src/app/middleware/cors.js
+++ js_serverless/src/app/middleware/cors.js
@@ -5,7 +5,7 @@ export default function corsMiddleware(req, res, next) {
 
   res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
 
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
   res.setHeader(
   "Access-Control-Allow-Headers",
